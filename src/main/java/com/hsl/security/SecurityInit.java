8aa7e429380a23325be9a0856f4eb82c07d2f0fb
LucHoang <hoangsyluc@gmail.com>
2021-05-11 14:55:43 +0700

    Phân quyền dựa trên vai trò

---
+++ src/main/java/com/hsl/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.hsl.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
